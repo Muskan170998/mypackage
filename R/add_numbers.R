bdd5e868c5eb26c917a634dd93056b5bccf85abf
Muskan170998 <muskan.priya@cheors.com>
2026-02-23 19:31:34 +0530

    Initial commit

---
+++ R/add_numbers.R
@@ -0,0 +1,9 @@
+#' Add two numbers
+#'
+#' @param x First number
+#' @param y Second number
+#' @return Sum x and y
+#' @export
+add_numbers<-function(x,y){
+  x+y
+  }
---
+++ tests/testthat/test-add_numbers.R
@@ -0,0 +1,3 @@
+test_that("add_numbers adds correctlt", {
+  expect_equal(add_numbers(5,8), 13)
+})
